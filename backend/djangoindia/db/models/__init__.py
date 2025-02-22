0b27b5f42a02ac8173e04cfbe0df461495bc110c
Anurag Yadav <yadavanurag1310@gmail.com>
2025-02-22 14:50:12 +0530

    added functionality of saving email to all the attendees

--- backend/djangoindia/db/models/__init__.py
+++ backend/djangoindia/db/models/__init__.py
@@ -1,5 +1,6 @@
 from .communication import ContactUs, Subscriber
 from .event import Event, EventRegistration, EventUserRegistration
+from .event_communication import EventCommunication
 from .partner_and_sponsor import CommunityPartner, Sponsor, Sponsorship
 from .update import Update
 from .user import SocialLoginConnection, User
@@ -19,4 +20,5 @@ __all__ = [
     "SocialLoginConnection",
     "Sponsor",
     "EventUserRegistration",
+    "EventCommunication",
 ]
