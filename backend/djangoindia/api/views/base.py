b82e6da4b3b15ebf1bea67ee25fac7b935289223
Anurag Yadav <yadavanurag1310@gmail.com>
2025-03-10 23:02:07 +0530

    pre-commit fixed

--- backend/djangoindia/api/views/base.py
+++ backend/djangoindia/api/views/base.py
@@ -8,6 +8,8 @@ from rest_framework import status
 from rest_framework.exceptions import APIException
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.settings import api_settings
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
@@ -29,7 +31,39 @@ class TimezoneMixin:
             timezone.deactivate()
 
 
-class BaseViewSet(TimezoneMixin, ModelViewSet):
+class PaginationMixin:
+    @property
+    def paginator(self):
+        """
+        The paginator instance associated with the view, or `None`.
+        """
+        if not hasattr(self, "_paginator"):
+            if self.pagination_class is None:
+                self._paginator = None
+            else:
+                self._paginator = self.pagination_class()
+        return self._paginator
+
+    def paginate_queryset(self, queryset):
+        """
+        Return a single page of results, or `None` if pagination
+        is disabled.
+        """
+        if self.paginator is None:
+            return queryset
+        return self.paginator.paginate_queryset(queryset, self.request, view=self)
+
+    def get_paginated_response(self, data):
+        """
+        Return a paginated style `Response` object for the given
+        output data.
+        """
+        if self.paginator is None:
+            return Response(data)
+        return self.paginator.get_paginated_response(data)
+
+
+class BaseViewSet(TimezoneMixin, ModelViewSet, PaginationMixin):
     model = None
 
     permission_classes = [
@@ -45,6 +79,8 @@ class BaseViewSet(TimezoneMixin, ModelViewSet):
 
     search_fields = []
 
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+
     def get_queryset(self):
         try:
             return self.queryset or self.model.objects.all()
@@ -52,7 +88,7 @@ class BaseViewSet(TimezoneMixin, ModelViewSet):
             raise APIException("Please check the view", status.HTTP_400_BAD_REQUEST)
 
 
-class BaseAPIView(TimezoneMixin, APIView):
+class BaseAPIView(TimezoneMixin, APIView, PaginationMixin):
     permission_classes = [
         IsAuthenticated,
     ]
@@ -65,6 +101,8 @@ class BaseAPIView(TimezoneMixin, APIView):
     filterset_fields = []
 
     search_fields = []
+
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
 
     def filter_queryset(self, queryset):
         for backend in list(self.filter_backends):
